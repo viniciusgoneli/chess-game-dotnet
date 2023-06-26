ba232144e5552fdff218db5e2db3e1a968f82f9a
viniciusgoneli <viniciusgoneli_2012@hotmail.com>
2023-06-26 11:06:15 -0300

    feat: created 'Board' layer and 'Position' class.

---
+++ xadrez/Board/Position.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace xadrez.Board
+{
+	public class Position
+	{
+		public int Row { get; set; }
+		public int Column { get; set; }
+
+		public Position(int row, int column)
+		{
+			Row = row;
+			Column = column;
+		}
+
+        public override string ToString()
+        {
+			return $"{Row}, {Column}";
+        }
+    }
+}
+
---
+++ xadrez/Program.cs
@@ -0,0 +1,6 @@
+﻿using xadrez.Board;
+
+Position pos = new Position(1, 3);
+
+Console.WriteLine(pos);
+
