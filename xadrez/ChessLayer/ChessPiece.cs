d084313912b1d3eff3974a59a18cdfb25e74c007
viniciusgoneli <viniciusgoneli_2012@hotmail.com>
2023-06-28 21:33:33 -0300

    feat: created 'ChessPiece class'

---
+++ xadrez/ChessLayer/ChessPiece.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using xadrez.BoardLayer;
+
+namespace xadrez.ChessLayer
+{
+	public abstract class ChessPiece : Piece
+	{
+		public ChessPiece(Board board, Color color) : base(board, color)
+		{
+		}
+	}
+}
+
