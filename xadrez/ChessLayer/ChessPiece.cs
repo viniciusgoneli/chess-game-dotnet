88ab58da472ae2898af475acf40ff95223d71c04
viniciusgoneli <viniciusgoneli_2012@hotmail.com>
2023-06-30 20:27:19 -0300

    feat: created ChessPiece new function.

--- xadrez/ChessLayer/ChessPiece.cs
+++ xadrez/ChessLayer/ChessPiece.cs
@@ -7,7 +7,16 @@ namespace xadrez.ChessLayer
 	{
 		public ChessPiece(Board board, Color color) : base(board, color)
 		{
-		}
-	}
+        }
+
+        protected void MarkOpponentPieceAsPossibleMoveIfExists(Position p, bool[,] mat)
+        {
+            if (Board.CheckPositionIsInsideBounds(p) && Board.IsPositionOccupied(p)
+                && Board.GetPiece(p).Color != Color)
+            {
+                mat[p.Row, p.Column] = true;
+            }
+        }
+    }
 }
 
