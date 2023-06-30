a97ba014dcf6302165f24eb95bad31a0df4fb0e6
viniciusgoneli <viniciusgoneli_2012@hotmail.com>
2023-06-30 20:28:20 -0300

    feat: Added 'PossibleMoves()' function to Piece.

--- xadrez/BoardLayer/Piece.cs
+++ xadrez/BoardLayer/Piece.cs
@@ -1,20 +1,28 @@
 ﻿using System;
 namespace xadrez.BoardLayer
 {
-	public  class Piece
+	public abstract class Piece
 	{
 		public Position? Position { get; set; }
 		public Color Color { get; protected set; }
-		public int movementsCount { get; protected set; }
 		public Board Board { get; protected set; }
+        public int MovesCount { get; set; }
 
-		public Piece(Board board, Color color)
+        public Piece(Board board, Color color)
 		{
 			Position = null;
 			Board = board;
 			Color = color;
-			movementsCount = 0;
+			MovesCount = 0;
 		}
-	}
+
+		public abstract bool[,] GetPossibleMoves();
+
+		public bool GetPossibleMove()
+		{
+			return GetPossibleMoves()[Position.Row, Position.Column];
+		}
+
+    }
 }
 
