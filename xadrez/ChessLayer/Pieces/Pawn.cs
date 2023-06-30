267658e37be04ecfaff281d7568029864a186809
viniciusgoneli <viniciusgoneli_2012@hotmail.com>
2023-06-30 20:29:05 -0300

    feat: Implemented Pawn possible moves.

--- xadrez/ChessLayer/Pieces/Pawn.cs
+++ xadrez/ChessLayer/Pieces/Pawn.cs
@@ -14,6 +14,35 @@ namespace xadrez.ChessLayer
         {
             return "P";
         }
+
+        public override bool[,] GetPossibleMoves()
+        {
+            Position p = new Position(0, 0);
+
+            bool[,] mat = new bool[Board.Rows, Board.Columns];
+
+            int dir = Color == Color.White ? 1 : -1;
+
+            p.SetValues(Position.Row - 1 * dir, Position.Column);
+            if (Board.CheckPositionIsInsideBounds(p) && !Board.IsPositionOccupied(p))
+            {
+                mat[p.Row, p.Column] = true;
+
+                p.SetValues(p.Row - 1 * dir, p.Column);
+                if(MovesCount == 0 && !Board.IsPositionOccupied(p))
+                {
+                    mat[p.Row, p.Column] = true;
+                }
+            }
+
+            p.SetValues(Position.Row - 1 * dir, Position.Column + 1);
+            MarkOpponentPieceAsPossibleMoveIfExists(p, mat);
+
+            p.SetValues(Position.Row - 1 * dir, Position.Column - 1);
+            MarkOpponentPieceAsPossibleMoveIfExists(p, mat);
+
+            return mat;
+        }
     }
 }
 
