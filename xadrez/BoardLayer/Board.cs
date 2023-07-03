2a207ec1e4a7c813dce4f3d5ebb398bd78e76c0a
viniciusgoneli <viniciusgoneli_2012@hotmail.com>
2023-07-03 13:27:26 -0300

    feat: expanded Board class.'

--- xadrez/BoardLayer/Board.cs
+++ xadrez/BoardLayer/Board.cs
@@ -7,30 +7,52 @@ namespace xadrez.BoardLayer
 		public int Columns { get; set; }
 		private Piece[,] Pieces;
 
-		public Board(int rows, int columns)
+        public Board(int rows, int columns)
 		{
 			Rows = rows;
 			Columns = columns;
-			Pieces = new Piece[rows, columns]; 
-		}
+			Pieces = new Piece[rows, columns];
+        }
 
 		public Piece GetPiece(Position position)
 		{
 			return Pieces[position.Row, position.Column];
 		}
 
-		public void AddPiece(Piece piece, Position position)
+        public Piece GetPiece(int row, int column)
+        {
+            return Pieces[row, column];
+        }
+
+        public void AddPieceToPosition(Piece piece, Position position)
 		{
 			ValidatePosition(position);
 			Pieces[position.Row, position.Column] = piece;
+			piece.Position = position;
 		}
 
-		public bool IsPositionOccupied(Position pos)
+        public Piece RemovePieceFromPosition(Position position)
+        {
+			Piece p = Pieces[position.Row, position.Column];
+			if (p == null) return null;
+            Pieces[position.Row, position.Column] = null;
+			p.Position = null;
+
+			return p;
+        }
+
+        public bool IsThereAPiece(Position pos)
 		{
-			return GetPiece(pos) != null;
+            if (!CheckPositionIsInsideBounds(pos))
+            {
+                string msg = $"The position [{pos.Row},{pos.Column}] is outside the boundaries of the board.";
+                throw new BoardException(msg);
+            }
+
+            return GetPiece(pos) != null;
 		}
 
-		public bool CheckPositionIsInsideBounds(Position pos)
+        public bool CheckPositionIsInsideBounds(Position pos)
 		{
 			return pos.Row < Rows &&
 				pos.Row >= 0 &&
@@ -38,18 +60,20 @@ namespace xadrez.BoardLayer
 				pos.Column >= 0;
 		}
 
-		public void ValidatePosition(Position pos)
+		public bool ValidatePosition(Position pos)
 		{
 			if(!CheckPositionIsInsideBounds(pos))
 			{
 				string msg = $"The position [{pos.Row},{pos.Column}] is outside the boundaries of the board.";
 				throw new BoardException(msg);
 			}
-            if (IsPositionOccupied(pos))
+            if (IsThereAPiece(pos))
             {
-                string msg = $"The position [{pos.Row},{pos.Column}] is already occupied by another part.";
+                string msg = $"The position [{pos.Row},{pos.Column}] is already occupied by another piece.";
                 throw new BoardException(msg);
             }
+
+			return true;
         }
 	}
 }
