4c1f3e303c573698702004a4e2717aaa1a05188f
viniciusgoneli <viniciusgoneli_2012@hotmail.com>
2023-07-03 13:31:14 -0300

    feat: implemented 'Queen' possible moves.

--- xadrez/ChessLayer/Pieces/Queen.cs
+++ xadrez/ChessLayer/Pieces/Queen.cs
@@ -14,6 +14,85 @@ namespace xadrez.ChessLayer
         {
             return "Q";
         }
+
+        public override bool[,] GetPossibleMoves()
+        {
+            bool[,] mat = new bool[Board.Rows, Board.Columns];
+
+            Position p = new Position(Position.Row - 1, Position.Column);
+            while (Board.CheckPositionIsInsideBounds(p) && !Board.IsThereAPiece(p))
+            {
+                mat[p.Row, p.Column] = true;
+
+                p.SetValues(p.Row - 1, p.Column);
+            }
+            MarkOpponentPieceAsPossibleMoveIfExists(p, mat);
+
+            p = new Position(Position.Row + 1, Position.Column);
+            while (Board.CheckPositionIsInsideBounds(p) && !Board.IsThereAPiece(p))
+            {
+                mat[p.Row, p.Column] = true;
+
+                p.SetValues(p.Row + 1, p.Column);
+            }
+            MarkOpponentPieceAsPossibleMoveIfExists(p, mat);
+
+            p = new Position(Position.Row, Position.Column - 1);
+            while (Board.CheckPositionIsInsideBounds(p) && !Board.IsThereAPiece(p))
+            {
+                mat[p.Row, p.Column] = true;
+
+                p.SetValues(p.Row, p.Column - 1);
+            }
+            MarkOpponentPieceAsPossibleMoveIfExists(p, mat);
+
+            p = new Position(Position.Row, Position.Column + 1);
+            while (Board.CheckPositionIsInsideBounds(p) && !Board.IsThereAPiece(p))
+            {
+                mat[p.Row, p.Column] = true;
+
+                p.SetValues(p.Row, p.Column + 1);
+            }
+            MarkOpponentPieceAsPossibleMoveIfExists(p, mat);
+
+            p = new Position(Position.Row - 1, Position.Column - 1);
+            while (Board.CheckPositionIsInsideBounds(p) && !Board.IsThereAPiece(p))
+            {
+                mat[p.Row, p.Column] = true;
+
+                p.SetValues(p.Row - 1, p.Column - 1);
+            }
+            MarkOpponentPieceAsPossibleMoveIfExists(p, mat);
+
+            p = new Position(Position.Row - 1, Position.Column + 1);
+            while (Board.CheckPositionIsInsideBounds(p) && !Board.IsThereAPiece(p))
+            {
+                mat[p.Row, p.Column] = true;
+
+                p.SetValues(p.Row - 1, p.Column + 1);
+            }
+            MarkOpponentPieceAsPossibleMoveIfExists(p, mat);
+
+            p = new Position(Position.Row + 1, Position.Column + 1);
+            while (Board.CheckPositionIsInsideBounds(p) && !Board.IsThereAPiece(p))
+            {
+                mat[p.Row, p.Column] = true;
+
+                p.SetValues(p.Row + 1, p.Column + 1);
+            }
+            MarkOpponentPieceAsPossibleMoveIfExists(p, mat);
+
+            p = new Position(Position.Row + 1, Position.Column - 1);
+            while (Board.CheckPositionIsInsideBounds(p) && !Board.IsThereAPiece(p))
+            {
+                mat[p.Row, p.Column] = true;
+
+                p.SetValues(p.Row + 1, p.Column - 1);
+            }
+            MarkOpponentPieceAsPossibleMoveIfExists(p, mat);
+
+            return mat;
+        }
     }
 }
 
